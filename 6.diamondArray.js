4e1cd015c32eeab74e76c5db2cb649e1fc09be2d
DeepaRajkumar <deeparajkumar59@gmail.com>
2025-07-01 07:17:24 +0530

    check there is two type of problem

--- 6.diamondArray.js
+++ 6.diamondArray.js
@@ -27,8 +27,27 @@
 // 2nd row - 5 space 
 // 3rd row - 4 space
 
+// first i have written the code think i have written code with my own logic 
+// but i am wrong. keen observation makes your perfect . first i thing i have written code for above , but now i have written for below pattern 
+
+// 1 2 3 4 5 6 7
+//  2 3 4 5 6 7
+//   3 4 5 6 7 
+//    4 5 6 7
+//     5 6 7
+//      6 7
+//       7 
+//       7
+//      6 7
+//     5 6 7
+//    4 5 6 7
+//   3 4 5 6 7
+//  2 3 4 5 6 7
+// 1 2 3 4 5 6 7    
+  
+
 function pattern(n){ 
-    // upper triangle logic
+    // upper triangle logic 
     for(let row = 1; row <= n ; row++){
         let space = row - 1
         let line = "" 
@@ -42,7 +61,9 @@ function pattern(n){
         } 
         console.log(line)
     }  
-    // second triangle logic 
+    // second triangle logic  
+    // upper triangle print until 7 
+    // again lower triangle start to print with 7 
     for(let row =n; row >= 1; row--)
     {
         let space = row -1; 
@@ -59,4 +80,43 @@ function pattern(n){
 
 
 
-pattern(7)
+pattern(7) 
+
+// second  solution // see sanket used different logic for lower triangle (fact it is not different soltion after observation , now for lower triangle i am going to writen my own way after understanding logic from sanket video) 
+// dont always going with your logic. try to understand the other logic also  
+console.log ("Different pattern")
+function Timepattern(n){ 
+    // upper triangle logic 
+    for(let row = 1; row <= n ; row++){
+        let space = row - 1
+        let line = "" 
+        for(let j=1 ; j<=space; j++){
+            line += ' ' // important there is not space after space, change the pattern shape
+        }   
+        let num = row
+        while(num<=n){
+            line += num + " "; 
+            num++;
+        } 
+        console.log(line)
+    }   
+    //lower triangle start with  6 7 and 5 space 
+    // 5 6 7  with 4 space 
+    // 4 5 6 7 with 3 space
+    // for(let row = 1; row < n; row++) {
+    for(let row = 1; row < n-1; row++) {
+        let space = n-row -1 // 7 - 1-1 => 5 
+        let line = "" 
+        for (let j =1; j <= space; j++){
+            line += " "
+        } 
+        // here i need value start with 6  
+        let num = n - row ; // 7 -1 = 6 ; 7-2 = 6 
+        for(;num<=n; num++){
+            line += num+ " "
+        }
+        console.log(line)
+
+    }
+}
+Timepattern(7);
